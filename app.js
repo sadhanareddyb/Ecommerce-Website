9558a598d116c9829d97ad4c7f01aa13a056655e
sadhanareddyb <butreddysadhana@gmail.com>
2020-05-15 10:37:02 +0530

    created user schems nd applied JWT auth

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const { v1: uuidv1 } = require('uuid');
+require("dotenv").config();
+// import routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+// app
+const app = express();
+
+// db
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true 
+    })
+    .then(() => console.log("DB Connected"));
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+// routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
